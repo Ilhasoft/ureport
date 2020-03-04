c7fbe557d6c015131feefbbb8bb27b81edb33758
Matheus <matheus.msa27@gmail.com>
2020-03-04 20:08:50 -0300

    fix gunicorn

--- gunicorn/gunicorn.conf.py
+++ gunicorn/gunicorn.conf.py
@@ -4,7 +4,7 @@ import multiprocessing
 bind = '0.0.0.0:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = workers
-name = 'rapidpro'
+name = 'ureport'
 env = 'DJANGO_SETTINGS_MODULE=ureport.settings'
 proc_name = 'ureport'
 default_proc_name = proc_name
@@ -12,5 +12,5 @@ loglevel = 'debug'
 accesslog = 'gunicorn.access'
 errorlog = 'gunicorn.error'
 timeout = 120
-chdir = '/app'
+chdir = '/ureport'
 capture_output = True
